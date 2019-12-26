65dd1bbbf251f0025c68765e7bd9ed8f70f23f3e
gentilmente <egentilemontes@gmail.com>
2019-12-26 20:59:37 -0300

    templates

--- test.go
+++ test.go
@@ -1,56 +1,15 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
-// SitemapIndex struct
-type SitemapIndex struct {
-	Locations []string `xml:"sitemap>loc"`
-}
-
-// News data from xml
-type News struct {
-	Titles    []string `xml:"url>news>title"`
-	Keywords  []string `xml:"url>news>keywords"`
-	Locations []string `xml:"url>loc"`
-}
-
-// NewsMap k,v
-type NewsMap struct {
-	Keyword  string
-	Location string
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
 }
 
 func main() {
-	var s SitemapIndex
-	var n News
-	newsMap := make(map[string]NewsMap)
-
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	xml.Unmarshal(bytes, &s)
-
-	// range will iterate over data structure -- returns key and value
-	for _, Location := range s.Locations {
-		Location = strings.TrimSpace(Location)
-		resp, _ := http.Get(Location)
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes, &n)
-
-		for idx := range n.Keywords {
-			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
-		}
-	}
-
-	for idx, data := range newsMap {
-		fmt.Println("\n\n\n", idx)
-		fmt.Println("\n", data.Keyword)
-		fmt.Println("\n", data.Location)
-
-	}
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8000", nil)
 }
