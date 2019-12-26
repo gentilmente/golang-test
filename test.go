b7041eb45dacdc9ead6a20644a02ec318a68aa71
gentilmente <egentilemontes@gmail.com>
2019-12-26 13:23:04 -0300

    https://www.youtube.com/watch?v=-PATP8IZq5A

--- test.go
+++ test.go
@@ -1,23 +1,41 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type sitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprint(l.Loc)
+}
+
+/*
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1> hola </h1>")
 	fmt.Fprintf(w, "<p> %s  </p>", "sarasa")
-}
+}*/
 
 func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	stringBody := string(bytes)
-	fmt.Println(stringBody)
+	//stringBody := string(bytes)
 	resp.Body.Close()
 
-	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8000", nil)
+	var s sitemapIndex
+	xml.Unmarshal(bytes, &s)
+
+	fmt.Println(s.Locations)
+
+	//http.HandleFunc("/", indexHandler)
+	//http.ListenAndServe(":8000", nil)
 }
