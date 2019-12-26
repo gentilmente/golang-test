8888d7a65d1c5b2ae7ba1a98ecae8d997a138102
gentilmente <egentilemontes@gmail.com>
2019-12-26 13:06:55 -0300

    https://www.youtube.com/watch?v=ccANcNk8Dac

--- test.go
+++ test.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1> hola </h1>")
+	fmt.Fprintf(w, "<p> %s  </p>", "sarasa")
 }
 
 func main() {
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	stringBody := string(bytes)
+	fmt.Println(stringBody)
+	resp.Body.Close()
+
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8000", nil)
 }
