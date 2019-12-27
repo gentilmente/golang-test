3de26baab97e3cf89c1459efc97bb432b89a6d50
gentilmente <egentilemontes@gmail.com>
2019-12-26 21:22:37 -0300

    16 https://www.youtube.com/watch?v=Uo9MSE2Gbzs

--- test.go
+++ test.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+type newsAggPage struct {
+	Title string
+	News  string
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+	p := newsAggPage{Title: "amazing", News: "some news"}
+	t, _ := template.ParseFiles("basictemplating.html")
+	fmt.Println(t.Execute(w, p))
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
@@ -11,5 +23,6 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
 	http.ListenAndServe(":8000", nil)
 }
