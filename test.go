50fbb11545ee3fcc73224876839e7f43d032131f
gentilmente <egentilemontes@gmail.com>
2019-12-26 22:17:35 -0300

    https://pythonprogramming.net/go/template-news-aggregator-go-language-programming-tutorial/

--- test.go
+++ test.go
@@ -1,18 +1,59 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
-	"html/template"
+	"io/ioutil"
 	"net/http"
+	"strings"
+	"text/template"
 )
+
+// SitemapIndex struct
+type SitemapIndex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+// News data from xml
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+// NewsMap k,v
+type NewsMap struct {
+	Keyword  string
+	Location string
+}
 
 type newsAggPage struct {
 	Title string
-	News  string
+	News  map[string]NewsMap
 }
 
 func newsAggHandler(w http.ResponseWriter, r *http.Request) {
-	p := newsAggPage{Title: "amazing", News: "some news"}
+	var s SitemapIndex
+	var n News
+	newsMap := make(map[string]NewsMap)
+
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+
+	// range will iterate over data structure -- returns key and value
+	for _, Location := range s.Locations {
+		Location = strings.TrimSpace(Location)
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+
+		for idx := range n.Keywords {
+			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	p := newsAggPage{Title: "amazing", News: newsMap}
 	t, _ := template.ParseFiles("basictemplating.html")
 	fmt.Println(t.Execute(w, p))
 }
