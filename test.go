6360c2428ab650fadf65196f8440ebc07de7d686
gentilmente <egentilemontes@gmail.com>
2019-12-28 17:01:49 -0300

    https://www.youtube.com/watch?v=HRKfVjCB3lM

--- test.go
+++ test.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+var wg sync.WaitGroup
+
+func say(s string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(time.Millisecond * 30)
+	}
+	wg.Done()
+}
+
+func main() {
+	wg.Add(1)
+	go say("hello")
+	wg.Add(1)
+	go say("goodbye")
+	wg.Wait()
+
+	wg.Add(1)
+	go say("hi")
+	wg.Wait()
 }
