45dcbcbd6b4751c674f747529c33fa57a5b3bb50
gentilmente <egentilemontes@gmail.com>
2019-12-28 19:02:39 -0300

    buffered channels

--- test.go
+++ test.go
@@ -1,69 +1,16 @@
 package main
 
-import (
-	"encoding/xml"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
-	"text/template"
-)
-
-// SitemapIndex struct
-type SitemapIndex struct {
-	Locations []string `xml:"sitemap>loc"`
-}
-
-// News data from xml
-type News struct {
-	Titles    []string `xml:"url>news>title"`
-	Keywords  []string `xml:"url>news>keywords"`
-	Locations []string `xml:"url>loc"`
-}
-
-// NewsMap k,v
-type NewsMap struct {
-	Keyword  string
-	Location string
-}
-
-type newsAggPage struct {
-	Title string
-	News  map[string]NewsMap
-}
-
-func newsAggHandler(w http.ResponseWriter, r *http.Request) {
-	var s SitemapIndex
-	var n News
-	newsMap := make(map[string]NewsMap)
-
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	xml.Unmarshal(bytes, &s)
-
-	// range will iterate over data structure -- returns key and value
-	for _, Location := range s.Locations {
-		Location = strings.TrimSpace(Location)
-		resp, _ := http.Get(Location)
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes, &n)
-
-		for idx := range n.Keywords {
-			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
-		}
-	}
-
-	p := newsAggPage{Title: "amazing", News: newsMap}
-	t, _ := template.ParseFiles("basictemplating.html")
-	fmt.Println(t.Execute(w, p))
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/agg/", newsAggHandler)
-	http.ListenAndServe(":8000", nil)
+
+	messages := make(chan string, 2)
+
+	messages <- "buffered"
+	//messages <- "buff"
+	messages <- "channel"
+
+	//fmt.Println(<-messages)
+	fmt.Println(<-messages)
+	fmt.Println(<-messages)
 }
